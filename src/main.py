c6adadc299d21ddd30fc2a9aafcbcb46b846c389
jfaverill <jfaverill@gmail.com>
2025-04-27 15:46:56 -0500

    Implement HTMLNode

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode
 
 def main():
     tn = TextNode("This is some anchor text",TextType.LINK , "https://www.boot.dev")
     print(tn)
+
+    hn = HTMLNode("p", "This is a paragraph", None, {"href": "https://www.google.com", "target": "_blank"})
+    print(hn)
 
 main()
