2db8c090e856977a2b154a7e5198063487bc0c86
jfaverill <jfaverill@gmail.com>
2025-05-06 17:14:19 -0500

    Add text_node_to_html_node function and tests

---
+++ src/test_main.py
@@ -0,0 +1,20 @@
+import unittest
+from main import text_node_to_html_node
+from textnode import TextNode, TextType
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_bold(self):
+        node = TextNode("This text should have bold tags", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This text should have bold tags")
+        self.assertEqual(html_node.to_html(), "<b>This text should have bold tags</b>")
+
+if __name__ == "__main__":
+    unittest.main()
