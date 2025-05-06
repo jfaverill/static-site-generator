848d2a13364569a70a7647ee3430021af6cd1a4f
jfaverill <jfaverill@gmail.com>
2025-05-06 17:13:07 -0500

    Implement ParentNode and tests

---
+++ src/parentnode.py
@@ -0,0 +1,16 @@
+from htmlnode import HTMLNode
+from leafnode import LeafNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props = None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
