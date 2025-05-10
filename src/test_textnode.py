614248980c7be85382b5d4689a8bab4407731ae4
jfaverill <jfaverill@gmail.com>
2025-05-10 12:10:40 -0500

    Refactor TextNode and tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
-
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -29,6 +28,19 @@ class TestTextNode(unittest.TestCase):
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
 
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_bold(self):
+        node = TextNode("This text should have bold tags", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This text should have bold tags")
+        self.assertEqual(html_node.to_html(), "<b>This text should have bold tags</b>")
 
 if __name__ == "__main__":
     unittest.main()
