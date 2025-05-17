c9049ab67e58b0e76fce3e2b383711fca97fcee5
jfaverill <jfaverill@gmail.com>
2025-05-17 12:52:10 -0500

    Implement text_to_textnodes and tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -71,3 +71,12 @@ def split_nodes_link(old_nodes):
             if len(old_node_text) > 0:
                 new_nodes.append(TextNode(old_node_text, old_node.text_type))
     return new_nodes
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
