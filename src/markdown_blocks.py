380a0a9f715e0c92f6c00e9edfe07a37ccd212c5
jfaverill <jfaverill@gmail.com>
2025-05-17 20:34:37 -0500

    Implement block_to_blocktype and tests

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,29 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERDED_LIST = "ordered_list"
+
+def block_to_blocktype(block_markdown):
+    heading_match = re.search(r"^#{1,6} ", block_markdown)
+    if heading_match:
+        return BlockType.HEADING
+    #code_block_match = re.search(r"^```.+```$", block_markdown)
+    if block_markdown.startswith("```") and block_markdown.endswith("```"):
+        return BlockType.CODE
+    if block_markdown.startswith(">"):
+        return BlockType.QUOTE
+    if block_markdown.startswith("- "):
+        return BlockType.UNORDERED_LIST
+    if block_markdown.startswith("1. "):
+        return BlockType.ORDERDED_LIST
+    return BlockType.PARAGRAPH
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     new_blocks = []
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_blocktype, BlockType
 
 class TestMarkdownToHTML(unittest.TestCase):
     ##################################
@@ -47,3 +47,30 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+    ##################################
+    # block_to_blocktype Tests
+    ##################################
+    def test_block_to_blocktype_heading_3(self):
+        block_md = "### This is a heading 3"
+        block_type = block_to_blocktype(block_md)
+        self.assertEqual(BlockType.HEADING, block_type)
+
+    def test_block_to_blocktype_heading_1(self):
+        block_md = "# This is a heading 1"
+        block_type = block_to_blocktype(block_md)
+        self.assertEqual(BlockType.HEADING, block_type)
+
+    def test_block_to_blocktype_heading_6(self):
+        block_md = "###### This is a heading 6"
+        block_type = block_to_blocktype(block_md)
+        self.assertEqual(BlockType.HEADING, block_type)
+
+    def test_block_to_blocktype_heading_7(self):
+        block_md = "####### This should end up being a paragraph"
+        block_type = block_to_blocktype(block_md)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_block_to_blocktype_codeblock(self):
+        block_md = "``` This should end up being a paragraph```"
+        block_type = block_to_blocktype(block_md)
+        self.assertEqual(BlockType.CODE, block_type)
