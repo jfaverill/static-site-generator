960ec976e5ec00e901d9f3026f5b406ca0c74db5
jfaverill <jfaverill@gmail.com>
2025-05-18 18:07:35 -0500

    Add TextNode class comments

--- src/textnode.py
+++ src/textnode.py
@@ -10,19 +10,22 @@ class TextType(Enum):
     IMAGE = "image"
 
 class TextNode():
+    # TextNode constructor
+    # takes in text and TextType Enum member as required
+    # url paramenter is optional (used for when TextNode is image or link)
     def __init__(self, text, text_type, url = None):
         self.text = text
         self.text_type = text_type
         self.url = url
-
+    # method to determine if two TextNode objects are equal
     def __eq__(self, other):
         return (self.text == other.text 
                 and self.text_type == other.text_type 
                 and self.url == other.url)
-
+    # method to return string representaion of TextNode object
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
-    
+   
 def text_node_to_html_node(text_node):
     match text_node.text_type:
         case TextType.TEXT:
