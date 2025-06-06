ed52aceab9bb58a9aed4f531b854a9a025311f0d
jfaverill <jfaverill@gmail.com>
2025-06-06 18:32:05 -0500

    Refactor copy_static

--- src/main.py
+++ src/main.py
@@ -1,11 +1,3 @@
-# from textnode import TextNode, TextType
-# from htmlnode import HTMLNode, LeafNode
-# from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
-# from markdown_blocks import (create_heading_html, 
-#                              create_codeblock_html, 
-#                              create_quote_html,
-#                              create_list_html,
-#                              create_paragraph_html)
 import os, shutil
 
 def copy_static(source_path):
@@ -20,14 +12,13 @@ def copy_static(source_path):
     dir_items = os.listdir(source_path)
     for item in dir_items:
         to_copy = os.path.join(source_path, item)
-        print(to_copy)
-        #shutil.copy(to_copy, "public")
+        target_path = to_copy.replace("static/", "")
         if not os.path.isfile(to_copy):
-            new_dir = os.path.join("public", to_copy.replace("static/", ""))
+            new_dir = os.path.join("public", target_path)
             os.mkdir(new_dir)
             copy_static(to_copy)
         else:
-            shutil.copy(to_copy, os.path.join("public", to_copy.replace("static/", "")))
+            shutil.copy(to_copy, os.path.join("public", target_path))
 
 def main():
     copy_static(source_path = "public")
