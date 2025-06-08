2c5041ea83042755f19c4871736f564398d940ba
jfaverill <jfaverill@gmail.com>
2025-06-08 14:22:53 -0500

    More copy_static refactoring

--- src/main.py
+++ src/main.py
@@ -1,26 +1,30 @@
 import os, shutil
 
-def copy_static(source_path):
-    if source_path == "public":
-        if os.path.exists(source_path):
-            shutil.rmtree(source_path)
-            os.mkdir(source_path)
-        else:
-            os.mkdir(source_path)
-        source_path = "static"
+def copy_static(source_path, target_path):
+    if not os.path.exists(target_path):
+        os.mkdir(target_path)
 
-    dir_items = os.listdir(source_path)
-    for item in dir_items:
-        to_copy = os.path.join(source_path, item)
-        target_path = to_copy.replace("static/", "")
-        if not os.path.isfile(to_copy):
-            new_dir = os.path.join("public", target_path)
-            os.mkdir(new_dir)
-            copy_static(to_copy)
+    source_items = os.listdir(source_path)
+    for item in source_items:
+        from_path = os.path.join(source_path, item)
+        to_path = os.path.join(target_path, item)
+        print(f" * {from_path} --> {to_path}")
+        if not os.path.isfile(from_path):
+            copy_static(from_path, to_path)
         else:
-            shutil.copy(to_copy, os.path.join("public", target_path))
+            shutil.copy(from_path, to_path)
 
 def main():
-    copy_static(source_path = "public")
+
+    source_path = "./static"
+    target_path = "./public"
+
+    # remove the target public directory and it's contents
+    print(f"Deleting path \"{target_path}\"...")
+    if os.path.exists(target_path):
+        shutil.rmtree(target_path)
+
+    print(f"Copying contents from \"{source_path}\" to \"{target_path}\"")
+    copy_static(source_path, target_path)
 
 main()
